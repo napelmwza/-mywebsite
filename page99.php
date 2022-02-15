b600e067fdedf701eea53607c8848ae685cd0a02
napelmwza <97940694+napelmwza@users.noreply.github.com>
2022-02-15 14:49:59 +0700

    Create page99.php

---
+++ page99.php
@@ -0,0 +1,53 @@
+<html>
+  <head>
+    <title>ตารางของฉัน</title>
+  </head>
+  <body>
+    <table border="1">
+      <tr>
+        <th>ลำดับ</th>
+        <th>ชื่อ - นามสกุล</th>
+        <th>ชื่อเล่น</th>
+        <th>รูป</th>
+        <th>ทวิตเตอร์</th>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>yu jimin</td>
+        <td>karina</td>
+        <td><img src="https://i.pinimg.com/originals/c5/0a/46/c50a46f12c99102d113ed344fa6390d0.jpg" width="100px"></td>
+        <td><a href="https://twitter.com/BLACKPINK">twitter</a></td>
+        <tr>
+        <td>2</td>
+        <td>kim minjeonge</td>
+        <td>winter</td>
+        <td><img src="https://pbs.twimg.com/media/FKHE8RqVgAIFhyk?format=jpg&name=large" width="100px"></td>
+        <td><a href="https://twitter.com/aespa_official">twitter</a></td>
+        <tr>
+        <td>3</td>
+        <td>noh jihye</td>
+        <td>noze</td>
+        <td><img src="https://pbs.twimg.com/media/FCdlWZoVQAUUPn6?format=jpg&name=large" width="100px"></td>
+        <td><a href="https://mobile.twitter.com/noze_world">twitter</a></td>
+        <tr>
+        <td>4</td>
+        <td>bae juhyun</td>
+        <td>irene</td>
+        <td><img src="https://pbs.twimg.com/media/FHqjMq0aAAAxhze?format=jpg&name=4096x4096" width="100px"></td>
+        <td><a href="https://mobile.twitter.com/rvsmtown">twitter</a></td>
+        <tr>
+        <td>5</td>
+        <td>kim taeyeon</td>
+        <td>taeyeon</td>
+        <td><img src="https://pbs.twimg.com/media/FK-Ux71aUAEgcOK?format=jpg&name=4096x4096" width="100px"></td>
+        <td><a href="https://mobile.twitter.com/girlsgeneration">twitter</a></td>
+        <tr>
+        <td>6</td>
+        <td>kim sunwoo</td>
+        <td>suwoo</td>
+        <td><img src="https://pbs.twimg.com/media/FJzBz-XVcAAD-t0?format=jpg&name=large" width="100px"></td>
+        <td><a href="https://mobile.twitter.com/enhypen_members">twitter</a></td>
+      </tr>
+    </table>
+  </body> 
+</html>
