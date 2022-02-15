8e9d14af160930c6b4c955feb081df73a3210e7f
napelmwza <97940694+napelmwza@users.noreply.github.com>
2022-02-15 14:58:03 +0700

    Create pag2.php

---
+++ pag2.php
@@ -0,0 +1,24 @@
+<html>
+	<head>
+		<title>การตกแต่งเว็บไซต์ด้วย CSS</title>
+		<style>
+			#box1{
+				width: 800px;
+				height: 350px;
+			}
+			.box{
+				background-color: blue;
+			}
+			body{
+                                background-color: lightpink;
+			}
+			h1{
+				color: yellow;      
+		</style>
+	</head>
+	<body>
+		<div id="box1" class="box">
+			<h1>นางสาวชนกนันท์ เรืองอยู่ ม.4/13 เลขที่ 19</h1>
+		</div>
+	</body>
+</html>
