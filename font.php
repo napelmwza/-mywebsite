521bd2ff857290b2464730095a916af4564a720d
napelmwza <97940694+napelmwza@users.noreply.github.com>
2022-02-22 19:10:30 +0700

    Create font.php

---
+++ font.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>กำหนดรูปแบบตัวอักษร</title>
+  <style>
+    @font-face{
+      font-family: myFont1;
+      src: url()
+    }
+    @font-face{
+      font-family: myFont2;
+      src: url()
+    }
+    #font1{
+	font-family: myFont1;
+    }
+    #font2{
+	font-family: myFont2;
+    }
+  </style>
+</head>
+<body>
+  <h1 id="font1">นางสาวชนกนันท์ เรืองอยู่ ม.4/13 เลขที่ 19 เลขประจำตัว 57030</h1>
+  <h1 id="font2">นางสาวชนกนันท์ เรืองอยู่ ม.4/13 เลขที่ 19 เลขประจำตัว 57030</h1>
+</body>
+</html>
